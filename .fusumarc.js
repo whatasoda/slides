f32cc05bc1076c00c65e070912f0d6b72c8cd07c
whatasoda <a16dc107@dhw.ac.jp>
2019-06-24 23:23:57 +0900

    add hello

---
+++ .fusumarc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  meta: {
+    author: 'Shota Hatada',
+    siteName: "whatasoda's slides",
+    repositoryUrl: 'https://github.com/whatasoda/slides',
+    sns: ['twitter', 'hatena']
+  }
+};
---
+++ slides/hello/slides/01-introduce.md
@@ -0,0 +1,3 @@
+# ME 😣
+
+whatasoda
